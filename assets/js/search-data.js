e4f2e833045d7ebbb0dddfc6d0c847d6a50b7663
avis1234 <avisegal@gmail.com>
2025-10-04 19:05:28 +0000

    Deploying to gh-pages from @ avisega/avisega.github.io@997a1159b4953dfe4dbf1d6698fccc573eda844f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "Selected collection of key projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
@@ -384,51 +391,36 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-detecting-struggling-student-programmers-using-proficiency-taxonomies",
+          title: 'Detecting Struggling Student Programmers using Proficiency Taxonomies',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/detecting-struggling-students-proficiency-taxonomies/";
+            },},{id: "projects-dual-mode-explanations-for-suicide-risk-detection-in-online-mental-health-support",
+          title: 'Dual-Mode Explanations for Suicide Risk Detection in Online Mental Health Support',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/dual-mode-explanations/";
+            },},{id: "projects-predicting-bug-fix-time-in-students-programming-with-deep-language-models",
+          title: 'Predicting Bug Fix Time in Students’ Programming with Deep Language Models',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/predicting-bug-fix-time/";
+            },},{id: "projects-predicting-suicide-risk-in-online-counseling-services",
+          title: 'Predicting Suicide Risk in Online Counseling Services',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+              window.location.href = "/projects/predicting-suicide-risk-online-counseling/";
+            },},{id: "projects-qblooms-teacher-in-the-loop-mcq-generation-aligned-with-bloom-s-taxonomy",
+          title: 'QBlooms: Teacher-in-the-Loop MCQ Generation Aligned with Bloom’s Taxonomy',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+              window.location.href = "/projects/qblooms-teacher-in-the-loop-mcqs/";
+            },},{id: "projects-transparency-check-designing-and-auditing-transparency-in-ai-based-personalization-systems",
+          title: 'Transparency-Check: Designing and Auditing Transparency in AI-Based Personalization Systems',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/transparency-check-ai-personalization/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
